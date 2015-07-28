b2e885d0f21ef7b965e62434225ee858c2960850
sergey-ivlev <isergey.git@gmail.com>
2015-07-28 13:31:07 +0300

    order by, atom opts, bugfixes

--- src/codd_pg_driver_utils.erl
+++ src/codd_pg_driver_utils.erl
@@ -12,7 +12,7 @@
 
 %% API
 -export([typecast_args/1, typecast_args/2]).
--export([primary_data/1, insert_data/1, insert_args/1, db_keys/1]).
+-export([primary_data/1, insert_data/1, insert_args/1, db_keys/1, db_keys/2]).
 -export([join_and_with_count/1, join_and_with_count/2]).
 -export([where/1, where/2, set_values/1]).
 -export([opts/1]).
@@ -50,7 +50,12 @@ insert_data({Module, _Meta, Data}) ->
                     false -> {Acc, Errors}
                 end;
             _ ->
-                {maps:put(K, V, Acc), Errors}
+                case codd_model:find_alias(Module, K, V) of
+                    {ok, {V1, _}} ->
+                        {maps:put(K, V1, Acc), Errors};
+                    {error, _} ->
+                        {Acc, [codd_error:alias_error(K) | Errors]}
+                end
         end
     end,
     case maps:fold(Fun,{maps:new(), []},Data) of
@@ -65,12 +70,22 @@ db_keys(Model) ->
         _ ->
             Table = Model:db_table(),
             <<$,, BinResult/binary>> =
-                << <<$,, Table/binary, ".", F/binary>> || F <- Keys>>,
+                << <<$,, Table/binary, ".\"", F/binary,"\" ">> || F <- Keys>>,
+            {ok, BinResult}
+    end.
+db_keys(Keys, Model) ->
+    ModelKeys = codd_model:db_keys(Keys, Model),
+    case ModelKeys of
+        [] -> {error, no_db_keys};
+        _ ->
+            Table = Model:db_table(),
+            <<$,, BinResult/binary>> =
+                << <<$,, Table/binary, ".\"", F/binary,"\" ">> || F <- ModelKeys>>,
             {ok, BinResult}
     end.
 
 opts(Opts) ->
-   LimitResult =
+    LimitResult =
         case maps:find(limit, Opts) of
             {ok, Limit} ->
                 limit(Limit);
@@ -88,14 +103,25 @@ opts(Opts) ->
                 end,
             case OffsetResult of
                 {ok, OffsetBin} ->
-                    {ok, <<LimitBinary/binary, OffsetBin/binary>>};
+                    OrderByResult =
+                        case maps:find(order_by, Opts) of
+                            {ok, OrderBy} ->
+                                order_by(OrderBy);
+                            error ->
+                                {ok, <<>>}
+                        end,
+                    case OrderByResult of
+                        {ok, OrderByBin} ->
+                            {ok, <<OrderByBin/binary, LimitBinary/binary, OffsetBin/binary>>};
+                        Error ->
+                            Error
+                    end;
                 Error ->
                     Error
             end;
         Error ->
             Error
     end.
-
 
 limit(N) when is_integer(N) ->
     {ok, <<" LIMIT ",(integer_to_binary(N))/binary>>};
@@ -105,6 +131,10 @@ offset(N) when is_integer(N) ->
     {ok, <<" OFFSET ",(integer_to_binary(N))/binary>>};
 offset(_) ->
     {error, codd_error:unvalid_error(offset)}.
+order_by({Field, desc}) ->
+    {ok, <<" ORDER BY \"",((atom_to_binary(Field, latin1)))/binary, "\" DESC ">>};
+order_by(_) ->
+    {error, codd_error:unvalid_error(order_by)}.
 
 where(Fields) ->
     where(1, Fields).
@@ -120,7 +150,7 @@ where(Count, IndexFV) ->
 insert_args(Fields) ->
     Fun = fun(K,_, {I, KeyAcc, IterationAcc}) ->
         {I+1,
-            <<KeyAcc/binary, ", ", (atom_to_binary(K, latin1))/binary>>,
+            <<KeyAcc/binary, ", \"", (atom_to_binary(K, latin1))/binary, "\"">>,
             <<IterationAcc/binary, ", $", (integer_to_binary(I))/binary>>
         }
     end,
@@ -140,7 +170,7 @@ join_and_with_count(FV) ->
 
 join_and_with_count(Count, FV) when is_map(FV) ->
     Fun = fun(K,_, {I, Acc}) ->
-        {I+1, <<Acc/binary, " AND ", (atom_to_binary(K, latin1))/binary, " = $", (integer_to_binary(I))/binary>>}
+        {I+1, <<Acc/binary, " AND \"", (atom_to_binary(K, latin1))/binary, "\" = $", (integer_to_binary(I))/binary>>}
     end,
     {NextCount, <<" AND ", TotalAcc/binary>>} = maps:fold(Fun, {Count, <<"">>}, FV),
     {NextCount, TotalAcc};
@@ -148,10 +178,11 @@ join_and_with_count(Count, FV) when is_map(FV) ->
 join_and_with_count(Count, FV) when is_list(FV) ->
     Fun = fun({Module, K,_}, {I, Acc}) ->
             Table = Module:db_table(),
-            {I+1, <<Acc/binary, " AND ", (Table)/binary, ".", (atom_to_binary(K, latin1))/binary, " = $", (integer_to_binary(I))/binary>>};
-        ({Module, K, Op, _}, {I, Acc}) when is_binary(Op) ->
+            {I+1, <<Acc/binary, " AND ", (Table)/binary, ".\"", (atom_to_binary(K, latin1))/binary, "\" = $", (integer_to_binary(I))/binary>>};
+        ({Module, K, Op, _}, {I, Acc}) ->
             Table = Module:db_table(),
-            {I+1, <<Acc/binary, " AND ", (Table)/binary, ".", (atom_to_binary(K, latin1))/binary, " ",Op/binary," $", (integer_to_binary(I))/binary>>}
+            BinOpt = op_to_bin(Op),
+            {I+1, <<Acc/binary, " AND ", (Table)/binary, ".\"", (atom_to_binary(K, latin1))/binary, "\" ",BinOpt/binary," $", (integer_to_binary(I))/binary>>}
     end,
     {NextCount, <<" AND ", TotalAcc/binary>>} = lists:foldl(Fun, {Count, <<"">>}, FV),
     {NextCount, TotalAcc}.
@@ -161,7 +192,7 @@ join_comma_with_count(FV) ->
 
 join_comma_with_count(Count, FV) when is_map(FV) ->
     Fun = fun(K,_, {I, Acc}) ->
-        {I+1, <<Acc/binary, " , ", (atom_to_binary(K, latin1))/binary, " = $", (integer_to_binary(I))/binary>>}
+        {I+1, <<Acc/binary, " , \"", (atom_to_binary(K, latin1))/binary, "\" = $", (integer_to_binary(I))/binary>>}
     end,
     {NextCount, <<" , ", TotalAcc/binary>>} = maps:fold(Fun, {Count, <<"">>}, FV),
     {NextCount, TotalAcc};
@@ -169,20 +200,28 @@ join_comma_with_count(Count, FV) when is_map(FV) ->
 join_comma_with_count(Count, FV) when is_list(FV) ->
     Fun = fun({Module, K,_}, {I, Acc}) ->
         Table = Module:db_table(),
-        {I+1, <<Acc/binary, " , ", (Table)/binary, ".", (atom_to_binary(K, latin1))/binary, " = $", (integer_to_binary(I))/binary>>}
+        {I+1, <<Acc/binary, " , ", (Table)/binary, ".\"", (atom_to_binary(K, latin1))/binary, "\" = $", (integer_to_binary(I))/binary>>}
     end,
     {NextCount, <<" , ", TotalAcc/binary>>} = lists:foldl(Fun, {Count, <<"">>}, FV),
     {NextCount, TotalAcc}.
 
 typecast_args(Args) ->
     Fun = fun({Module, Key, Value}, {Acc, Errors}) ->
-            case codd_typecast:typecast(Module, Key, Value) of
-                {ok, ValidValue} -> { [ValidValue | Acc], Errors};
-                {error, Error} ->  {Acc, [Error | Errors]}
-            end;
+        case codd_model:find_alias(Module, Key, Value) of
+            {ok, {FindedValue, _}} ->
+                case codd_typecast:typecast(Module, Key, FindedValue) of
+                    {ok, ValidValue} -> { [ValidValue | Acc], Errors};
+                    {error, Error} ->  {Acc, [Error | Errors]}
+                end;
+            {error, Error} ->  {Acc, [Error | Errors]}
+        end;
         ({Module, Key, _, Value}, {Acc, Errors}) ->
-            case codd_typecast:typecast(Module, Key, Value) of
-                {ok, ValidValue} -> { [ValidValue | Acc], Errors};
+            case codd_model:find_alias(Module, Key, Value) of
+                {ok, {FindedValue, _}} ->
+                    case codd_typecast:typecast(Module, Key, FindedValue) of
+                        {ok, ValidValue} -> { [ValidValue | Acc], Errors};
+                        {error, Error} ->  {Acc, [Error | Errors]}
+                    end;
                 {error, Error} ->  {Acc, [Error | Errors]}
             end
     end,
@@ -208,3 +247,11 @@ typecast_args(Module, Args) when is_map(Args) ->
             {error, Errors}
     end.
 
+op_to_bin(Op) ->
+    case Op of
+        '=' -> <<"=">>;
+        '>' -> <<">">>;
+        '<' -> <<"<">>;
+        '>=' -> <<">=">>;
+        '<=' -> <<"<=">>
+    end.
